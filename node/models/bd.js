ca45fc5fb2885aa46375488d41b6db95dcb2cab4
wal561606 <143214414+wal561606@users.noreply.github.com>
2023-09-19 09:32:19 -0300

    Update bd.js

--- node/models/bd.js
+++ node/models/bd.js
@@ -4,17 +4,17 @@ var util = require('util');
 // conexión a la base de datos mediante un pool 
 var pool = mysql.createPool({
     connectionLimit: 10,
-    //host: process.env.MYSQLHOST,
-    //user: process.env.MYSQLUSER, 
-    //password: process.env.MYSQLPASSWORD,
-    //database: process.env.MYSQLDATABASE,
-    //url: process.env.MYSQL_URL,
-    //port: process.env.MYSQLPORT
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER, 
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    url: process.env.MYSQL_URL,
+    port: process.env.MYSQLPORT
 
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER, 
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    //host: process.env.MYSQL_HOST,
+    //user: process.env.MYSQL_USER, 
+    //password: process.env.MYSQL_PASSWORD,
+    //database: process.env.MYSQL_DB_NAME
    
 })
 
